ac52e8608a58387ead4f1599d8ec739dec3cbe6e
Shaker Kattoula <smkattoula@gmail.com>
2020-06-19 22:17:05 -0700

    finish backend, work on Header for front-end pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from './components/layout/Header';
+import Home from './components/pages/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./style.css";
+
+export default function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Header /> 
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+            </BrowserRouter>
+        </>
+    )
 }
-
-export default App;
