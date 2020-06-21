e8daf67b0525ddeec92ca52ecddfdf4e47895032
Shaker Kattoula <smkattoula@gmail.com>
2020-06-21 12:40:00 -0700

    inital commit, style header and create login/register buttons

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AuthOptions from '../auth/AuthOptions';
 
 export default function Header() {
     return (
         <div>
-            Header
+            <header id="header">
+            <Link to ="/">
+                <h1 className="title">Ticket Tracker App</h1>
+            </Link>
+            <AuthOptions />
+            </header>
         </div>
     )
 }
