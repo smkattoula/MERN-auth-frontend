b0ee95825852a12881dca5d45b0a74881612bc99
Shaker Kattoula <smkattoula@gmail.com>
2020-06-23 15:47:43 -0700

    implement context userData

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Axios from 'axios';
 import Header from './components/layout/Header';
 import Home from './components/pages/Home';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import UserContext from './context/UserContext';
 
 import "./style.css";
 
 export default function App() {
+    const [userData, setUserData] = useState({
+        token: undefined,
+        user: undefined,
+    })
+
+    useEffect(() => {
+        const checkLoggedIn = async () => { 
+        let token = localStorage.getItem("auth-token");
+        if (token === null) {
+            localStorage.setItem("auth-token", "");
+            token = "";
+        }
+        const tokenResponse = await Axios.post(
+            "http://localhost:5000/users/tokenIsValid",
+            null,
+            { headers: { "x-auth-token": token } }
+        );
+        if (tokenResponse.data) {
+            const userResponse = await Axios.get("http://localhost:5000/users/",  { headers: { "x-auth-token": token } },
+            );
+            setUserData({
+            token,
+            user: userResponse.data,
+            });
+        }
+    };
+
+    checkLoggedIn();
+}, []);
+
     return (
         <>
             <BrowserRouter>
+            <UserContext.Provider value={{ userData, setUserData }}>
                 <Header /> 
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route path="/login" component={Login} />
                     <Route path="/register" component={Register} />
                 </Switch>
+                </UserContext.Provider>
             </BrowserRouter>
         </>
     )
